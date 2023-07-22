e81590a9303682522b3cd311cf0efa8e932e3976
Tanay Bhomia <bhomia123456789@gmail.com>
2023-07-22 11:13:49 +0530

    Formatted the output

---
+++ create_executable.py
@@ -0,0 +1,62 @@
+import os
+import platform
+import subprocess
+import shutil
+
+def add_to_path(bin_directory):
+    # Get the user's home directory
+    home_directory = os.path.expanduser("~")
+
+    # Get the current PATH variable value and split it into a list of directories
+    current_path = os.environ.get("PATH", "").split(os.pathsep)
+
+    # Check if the 'bin' directory is already in the PATH
+    if bin_directory not in current_path:
+        # Add the 'bin' directory to the beginning of the PATH list
+        updated_path = [bin_directory] + current_path
+
+        # Join the directories back into a string with the appropriate separator
+        updated_path_str = os.pathsep.join(updated_path)
+
+        # Update the PATH environment variable for the current session
+        os.environ["PATH"] = updated_path_str
+
+        # Save the updated PATH to the system environment variable permanently
+        if platform.system() == "Windows":
+            subprocess.run(["setx", "PATH", updated_path_str], shell=True)
+        else:
+            # For non-Windows systems, update the user's profile to add the PATH
+            profile_file = os.path.join(home_directory, ".profile")
+            with open(profile_file, "a") as f:
+                f.write(f"\nexport PATH={updated_path_str}")
+
+def create_expense_tracker_executable():
+    # Set the script name and output directory for the Expense Tracker
+    script_name = "expcli.py"  # This is the name of your main script
+    output_directory = "dist"
+
+    # Run PyInstaller to create the single-file executable
+    cmd = f'pyinstaller --onefile {script_name} --distpath "{output_directory}" --noconfirm --log-level ERROR'
+    subprocess.run(cmd, shell=True)
+
+    # Rename the executable (remove the .exe extension on non-Windows systems)
+    if platform.system() != "Windows":
+        shutil.move(os.path.join(output_directory, "expcli.exe"), os.path.join(output_directory, "main"))
+
+def main():
+    print("Creating Expense Tracker executable...")
+    create_expense_tracker_executable()
+
+    # Create 'bin' directory and copy the executable to it
+    bin_directory = os.path.join(os.path.expanduser("~"), "bin")
+    os.makedirs(bin_directory, exist_ok=True)
+    shutil.copy(os.path.join("dist", "expcli" if platform.system() != "Windows" else "expcli.exe"), bin_directory)
+
+    # Add the 'bin' directory to the system's PATH
+    print("Adding the Expense Tracker executable to the system's PATH...")
+    add_to_path(bin_directory)
+
+    print("Expense Tracker executable is now available on your system.")
+
+if __name__ == "__main__":
+    main()
---
+++ expcli.py
@@ -0,0 +1,149 @@
+from expense import Expense
+import datetime
+import calendar
+import shutil
+
+# Creates the Budget File for tracking the monthly budget 
+def get_budget_from_user_or_file():
+    budget_file_path = "budget.txt"
+    try:
+        with open(budget_file_path, "r") as budget_file:
+            budget = float(budget_file.readline().strip())
+    except (FileNotFoundError, ValueError):
+        budget = None
+
+    while budget is None:
+        try:
+            budget_input = input("Enter your monthly budget: ")
+            budget = float(budget_input)
+            with open(budget_file_path, "w") as budget_file:
+                budget_file.write(str(budget))
+        except ValueError:
+            print("Invalid input. Please enter a valid monthly budget as a number.")
+
+    return budget
+
+# Takes the User input for an Expense 
+def user_input():
+	print(f"\n✍️ ENTER THE EXPENSE")
+	expense_name = input("I Bought :")
+	expense_amount = float(input("For Rupees :"))
+	print(f"Your Expense is {expense_name} and {expense_amount}")
+
+	# defining categories 
+	expense_category = [
+		"🍔 Food",
+		"🏠 Home",
+		"👕 Cloth",
+		"💻 Work",
+		"🎼 Fun",
+		"🪣 Misc",
+	]
+
+	# for taking the category from the user
+	while True:
+
+		print("\n🔢 SELECT CATEGORY")
+		for i,category_name in enumerate(expense_category):
+			print(f"  {i+1} : {category_name}")
+
+		value_range = f"[1 - {len(expense_category)}]"
+		selected_index = int(input(f"Enter a Category {value_range} : ")) - 1
+		if selected_index in range(len(expense_category)):
+			selected_category = expense_category[selected_index]
+			newexpense = Expense(name = expense_name,category= selected_category,amount=expense_amount)
+			return newexpense
+		else : 
+			print("Invalid Entry, Please try again")
+
+# Saves the content to the CSV file
+def savetofile(expense : Expense,expense_file_path):
+	print(f"\n📁 SAVING USER EXPENSE : {expense}")
+	with open(expense_file_path,"a", encoding = 'utf-8') as f:
+		f.write(f"{expense.name},{expense.category},{expense.amount}\n")
+	pass
+
+# Shows the Summary of the Spending
+def summaries(expense_file_path,budget):
+	print(f"\n📓 SUMMARY")
+	expenses = []
+	with open(expense_file_path , "r" , encoding='utf-8') as f:
+		lines = f.readlines()
+		for line in lines:
+			expense_name,expense_category,exepense_amount = line.strip().split(",")
+			line_expense = Expense(
+				name = expense_name,
+				category = expense_category,
+				amount = float((exepense_amount))
+			)
+			expenses.append(line_expense)
+	
+	amount_by_category = {} 
+	for expense in expenses:
+		key = expense.category
+		if key in amount_by_category:
+			amount_by_category[key] += expense.amount
+		else:
+			amount_by_category[key] = expense.amount
+
+	print("Expenses by Category")
+	for category,amount in amount_by_category.items():
+		print(f"  {category}:,{amount:.2f}₹")
+	
+	total = float(sum(amount_by_category.values()))
+	remaining_budget = budget-total
+	print(f"\n🎰 You have spent a total of {total}")
+	if remaining_budget <= 0:
+		print(f"🫠 You have already spent over the budget")
+	else :
+		print(f"🎫 Remaining Budget {remaining_budget}")
+
+def main():
+    print(r'''
+ ______                                  _______             _
+|  ____|                                |__   __|           | |
+| |__  __  ___ __   ___ _ __  ___  ___     | |_ __ __ _  ___| | _____ _ __
+|  __| \ \/ / '_ \ / _ \ '_ \/ __|/ _ \    | | '__/ _` |/ __| |/ / _ \ '__|
+| |____ >  <| |_) |  __/ | | \__ \  __/    | | | | (_| | (__|   <  __/ |
+|______/_/\_\ .__/ \___|_| |_|___/\___|    |_|_|  \__,_|\___|_|\_\___|_|
+            | |
+            |_|  
+    ''')
+    
+	# Creates the Budget file 
+    budget = get_budget_from_user_or_file()
+    
+	# Creates the Expenses Database
+    expense_file_path = "expense.csv"
+    
+	# Options 
+    while True:
+        print("\n✅ WHAT DO YOU WANT TO DO")
+        print("1. 💸 Enter an Expense")
+        print("2. 📓 Show Summary")
+        print("3. 🔴 Exit")
+        choice = input("-> ")
+
+        if choice == "1":
+            # Takes the user input
+            expense = user_input()
+
+            # Saves the input to the file
+            savetofile(expense, expense_file_path)
+	    
+        elif choice == "2":
+            # Rd the file and then show the summary
+            summaries(expense_file_path, budget)
+	    
+        elif choice == "3":
+            print("EXITING THE EXPENSE TRACKER! BYE 🖐️")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            continue
+
+
+
+
+if __name__ == "__main__":
+	main()
